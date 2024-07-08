edab25fab174700b2b77e6017f5a8591a18d442f
turnMeUpSon <milleryan2003@gmail.com>
2024-07-07 23:51:51 -0100

    Updated 347. Top K Frequent Elements with more efficient Max Heap solution

--- arrays_and_hashing/top_k_frequent_elements/top_k_frequent_element.py
+++ arrays_and_hashing/top_k_frequent_elements/top_k_frequent_element.py
@@ -1,35 +1,34 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # define the frequency array that is more that the length of nums
-        # because we need [0] [1] [2] [3] [4] [5] [6]
-        # numbers are indexed by their frequency
-
-        freq = [[] for i in range(len(nums) + 1)]
+        hashmap = {}
+        max_heap = []
         result = []
-        counter = {}
 
-        # count the amount of each integer
         for i in range(len(nums)):
-            counter[nums[i]] = 1 + counter.get(nums[i], 0)
+            hashmap[nums[i]] = 1 + hashmap.get(nums[i], 0)
 
-        # iterate through the hashmap and add integers to freq array by index that is the frequency
-        for n, c in counter.items():
-            freq[c].append(n)
+        for num, freq in hashmap.items():
+            max_heap.append([-1 * freq, num])
 
-        for cell in range(len(freq) - 1, -1, -1):
-            for num in freq[cell]:
-                result.append(num)
+        heapq.heapify(max_heap)
 
-                if len(result) == k:
-                    return result
+        while k > 0:
+            # data = [freq, num]
+            data = heapq.heappop(max_heap)
+            num = data[1]
+            result.append(num)
+            k -= 1
+
+        return result
 
 
 nums = [1, 1, 1, 2, 2, 3]
 k = 2
 driver = Solution()
 print(
-    f"Example 1:\nInput: nums = {nums}, k = {k}\nOutput: {driver.topKFrequent(nums, k)}"
+    f"Example 1:\nInput: nums = {nums}, k = {k}\nOutput: {driver.topKFrequent(nums, k)}\n"
 )
