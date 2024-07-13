d6bfc811e413d9fdf85666365246d132594319ee
turnMeUpSon <milleryan2003@gmail.com>
2024-07-12 23:22:03 -0100

    Solved 35. Search Insert Position

---
+++ binary_search/search_insert_position/search_insert_position.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] > target:
+                r = mid - 1
+
+            else:
+                l = mid + 1
+
+        return l
+
+    # Time complexity: O(logn)
+    # Space Complexity: O(1)
