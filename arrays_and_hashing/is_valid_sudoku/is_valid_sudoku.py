22566147f44d499b40d611f3e923f372c76811f2
turnMeUpSon <milleryan2003@gmail.com>
2024-07-16 17:49:48 -0100

    Modified is_valid_sudoku

--- arrays_and_hashing/is_valid_sudoku/is_valid_sudoku.py
+++ arrays_and_hashing/is_valid_sudoku/is_valid_sudoku.py
@@ -1,39 +1,34 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for i in range(9)]  # List of sets for columns
-        cols = [set() for i in range(9)]  # List of sets for rows
-        boxes = {}  # hashmap of sets for 3x3 sub-boxes
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)  # (row // 3, col // 3)
 
         for row in range(9):
             for col in range(9):
-
                 if board[row][col] == ".":
                     continue
 
                 else:
-
-                    # Check for duplicates in the current row, column, and sub-box
                     if (
                         board[row][col] in rows[row]
                         or board[row][col] in cols[col]
-                        or board[row][col] in boxes.get((row // 3, col // 3), set())
+                        or board[row][col] in boxes[(row // 3, col // 3)]
                     ):
                         return False
 
-                    # Add the current number to the respective sets
-                    rows[row].add(board[row][col])
-                    cols[col].add(board[row][col])
-
-                    # Add to the sub-box set, create it if not exists
-                    if (row // 3, col // 3) not in boxes:
-                        boxes[(row // 3, col // 3)] = set()
-
-                    boxes[(row // 3, col // 3)].add(board[row][col])
+                rows[row].add(board[row][col])
+                cols[col].add(board[row][col])
+                boxes[(row // 3, col // 3)].add(board[row][col])
 
         return True
+
+    # Time: (1)
+    # Space: O(1)
 
 
 board1 = [
