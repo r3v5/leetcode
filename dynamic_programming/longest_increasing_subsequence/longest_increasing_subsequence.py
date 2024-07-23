34d45fa8029303d30b424cbc9819478a63fe33a7
turnMeUpSon <milleryan2003@gmail.com>
2024-07-23 12:37:41 -0100

    Solved 300. Longest Increasing Subsequence

---
+++ dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # Bottom up DP
+        dp = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
+
+    # Time: O(n ^ 2)
+    # Space: O(n)
